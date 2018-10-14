efe5c75eb0db72b40b8a3bfb21b6e39a5c12a323
Anders Eggers-Krag <aek@aek.dk>
2018-10-14 14:54:21 +0200

    added support for (de)serializion and fixed a few bugs

---
+++ SimplyJson/JsonIgnoreAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplyJson {
+	[AttributeUsage(AttributeTargets.Property)]
+	public class JsonIgnoreAttribute : Attribute {
+	}
+}
